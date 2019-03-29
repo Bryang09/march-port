c00864d4f58faf7b681899d7bb2871bd0852af5d
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-03-29 17:34:12 -0500

    Styled Info Component

--- src/Components/Landing/ProjectInfo/Info.js
+++ src/Components/Landing/ProjectInfo/Info.js
@@ -8,8 +8,22 @@ const Info = props => {
 
   return (
     <div className="info">
+      <h1 className="number">0{project.id}.</h1>
       <h1>{project.name}</h1>
       <h3>{project.desc}</h3>
+      <div className="buttons">
+        <a href={project.demo} target="_blank" rel="noopener noreferrer">
+          <h5>Demo</h5>
+        </a>
+        <a
+          href={project.code}
+          className="link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h5>Link</h5>
+        </a>
+      </div>
     </div>
   );
 };
--- src/Components/Landing/projects.js
+++ src/Components/Landing/projects.js
@@ -3,32 +3,42 @@ export const Projects = {
     id: 1,
     name: "The Fashion App",
     desc: "This is a MERN application intended to be all about men clothing",
-    class: "Clothing"
+    class: "Clothing",
+    code: "https://github.com/Bryang09/men-clothing",
+    demo: "https://thawing-citadel-89279.herokuapp.com"
   },
   watches: {
     id: 2,
     name: "The Watches App",
     desc: "This is an application created with the MERN Stack.",
-    class: "Watches"
+    class: "Watches",
+    code: "https://github.com/Bryang09/watches",
+    demo: "https://intense-bayou-29884.herokuapp.com/#/"
   },
   shelters: {
     id: 3,
     name: "The Shelter App",
     desc:
       "This is a React application that uses the PetFinder API to search for nearby shelters and animals.",
-    class: "Shelter"
+    class: "Shelter",
+    code: "https://github.com/Bryang09/newshelter",
+    demo: "https://bryang09.github.io/newshelter/#/"
   },
   movies: {
     id: 4,
     name: "The Movies App",
     desc:
       "This is an application that uses the MoviesDB API to search for movies and shows.",
-    class: "Movies"
+    class: "Movies",
+    code: "https://github.com/Bryang09/newmovies",
+    demo: "https://bryang09.github.io/newmovies/#/"
   },
   anime: {
     id: 5,
     name: "The Anime App",
     desc: "This application will search for any Anime of Manga",
-    class: "Anime"
+    class: "Anime",
+    code: "https://github.com/Bryang09/anime",
+    demo: "https://bryang09.github.io/anime/#/"
   }
 };
